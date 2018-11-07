7a3096595889823d24a1f7609f814189faec2436
Alexis Poupelin <codevirtuel@gmail.com>
2018-11-07 09:25:30 +0100

    Moved fidelity point calculation

--- TD Refactoring initial-code/src/v1/Rental.java
+++ TD Refactoring initial-code/src/v1/Rental.java
@@ -32,5 +32,11 @@ public class Rental {
 				result += (getDaysRented() - 3) * 1.5; break;
 		}
 		return result;
+	}
+
+	int getFrequentRenterPoints() {
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+			return 2;
+		return 1;
 	} 
 }
