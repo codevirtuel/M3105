1e50820e59a69c54f1172f0cd1431882eb2fb737
Alexis Poupelin <codevirtuel@gmail.com>
2018-11-07 10:20:35 +0100

    Moved getFrequentRenterPoints to Movie

--- TD Refactoring initial-code/src/v1/Rental.java
+++ TD Refactoring initial-code/src/v1/Rental.java
@@ -23,8 +23,6 @@ public class Rental {
 	}
 
 	int getFrequentRenterPoints() {
-		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
-			return 2;
-		return 1;
+		return getMovie().getFrequentRenterPoints(getDaysRented());
 	}
 }
